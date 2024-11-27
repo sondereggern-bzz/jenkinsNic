edd27069f3a3554723d4764d42e2fa56c5debee1
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-11-27 14:16:31 +0100

    n

---
+++ trafficlight.test.js
@@ -0,0 +1,16 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+    it(`case green light`, () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+    it(`case undefined light`, () => {
+        expect(app.trafficLight()).toBe("OUT OF ORDER");
+    });
+    it(`case orange light`, () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+});
